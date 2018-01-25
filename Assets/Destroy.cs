c801778b21a5deb8b335c64c0d2f9bf6dd976ee9
mayu <trachan15-wan4@nifty.com>
2018-01-25 22:16:16 +0900

    this.unitychan = GameObject.Find("unitychan");　　未だ消えません

--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -17,17 +17,21 @@ public class Destroy : MonoBehaviour {
 
         this.difference = unitychan.transform.position.z - this.transform.position.z;
 
+        // this.unitychan = GameObject.Find("unitychan");
+
+        unitychan = GameObject.Find("unitychan");
+
     }
 
     // Update is called once per frame
     void Update() {
 
-       // this.difference = unitychan.transform.position.z - this.transform.position.z;
+        // this.difference = unitychan.transform.position.z - this.transform.position.z;
 
-       // this.gameObject.transform.position < 10
-         //   this.difference = unitychan.transform.position.z - this.transform.position.z;
+        // this.gameObject.transform.position < 10
+        //   this.difference = unitychan.transform.position.z - this.transform.position.z;
 
-        
+
         // this.difference = unitychan.transform.position.z - this.transform.position.z;
 
         //  画面外に出たら破壊する
@@ -35,16 +39,18 @@ public class Destroy : MonoBehaviour {
         // if (transform.position.z　<　-5.0f)
         //  if (transform.position.z  <  this.difference
 
-       // if (gameObject.transform.position.z =< (unitychan.transform.position.z - this.transform.position.z))
+        // if (gameObject.transform.position.z =< (unitychan.transform.position.z - this.transform.position.z))
 
-       //if (gameObject.transform.position.z - this.transform.position.z） >= (unitychan.transform.position.z - this.transform.position.z))
-　
-      // if (gameObject.transform.position.z - transform.position.z）= > (unitychan.transform.position.z - transform.position.z))
+        //if (gameObject.transform.position.z - this.transform.position.z） >= (unitychan.transform.position.z - this.transform.position.z))
 
-       // if (gameObject.transform.position.z - transform.position.z）= < (unitychan.transform.position.z - 10.0f)
+        // if (gameObject.transform.position.z - transform.position.z）= > (unitychan.transform.position.z - transform.position.z))
+
+        // if (gameObject.transform.position.z - transform.position.z）= < (unitychan.transform.position.z - 10.0f)
 
 
-       if (unitychan.transform.position.z - gameObject.transform.position.z  >  10.0f)
+        
+
+        if (unitychan.transform.position.z - gameObject.transform.position.z  >  10.0f)
 
                     {
             Destroy(gameObject);
